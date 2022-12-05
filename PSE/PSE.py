6f8b1d3b7227485a57e42f3baffe2ba7f7d68fe1
Amir H. Amanzadi <47634201+amanzadi@users.noreply.github.com>
2022-12-05 20:47:43 +0330

    Update PSE.py

--- PSE/PSE.py
+++ PSE/PSE.py
@@ -9,13 +9,29 @@ import matplotlib.pyplot as plt
 
 # ================================ 2. Functions =================================
 
-fda_drugs, psa_graph
+look4smi, fda_drugs, psa_graph, psa_predict, smi2psa, pred2prot
 
+def loo2smi(SMILES):
+    
+# looks for FDA_drugs and 
 def fda_drug(drug_name):
+   
+    df_fda_drugs = pd.read_csv('../data/fda_drugs.csv', sep=',')
+    
     if drug_name == 'all':
-        return()
+        #dic_drug_names = {df_fda_drugs['DrugID']:df_fda_drugs['Drug_Names'] for drug in df_fda_drugs}
+        return(dic_drug_names['Drug_Names'].tolist())
+    
+    elif drug_name in dic_drug_names['Drug_Names'].tolist():
+        return(dic_drug_names.loc[dic_drug_names['Drug_Names'] == drug_name].value())
+    
+    elif drug_name not in dic_drug_names['Drug_Names'].tolist():
+        print('Can not find the drug name in our FDA library.\n Please use "look4smi" to search the FDA drug library with th SMILES string.')
+    
     else:
-        return()
+        print('Please enter the correct drug name. Or type "all" to see the entirety of the FDA drug list.')
+        
+  
 
 #Perdicts the DTI using pretrained models
 def calc_affinity(drug, target, pre_model='MPNN_CNN_DAVIS'):
